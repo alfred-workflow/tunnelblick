8e5f078ec5780f0a90e6c0e5bc0e5a2d929e9792
liangshen001 <wangliang520wl@hotmail.com>
2023-02-18 16:58:25 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import {run} from "@apple-jxa/node";
+import alfred from "@alfred-workflows/core";
+
+run(() => {
+    let application = Application("Tunnelblick");
+    application.includeStandardAdditions = true;
+    const configurations = []
+    const length = application.configurations().length
+    for (let i = 0; i < length; i++) {
+        const configuration = application.configurations.at(i);
+        configurations.push({
+            name: configuration.name(),
+            state: configuration.state()
+        });
+    }
+    return configurations;
+    // 不支持
+    // return application.configurations().map(i => ({
+    //     name: i.name(),
+    //     state: i.state()
+    // }))
+}).then(configurations => {
+    const items = configurations.map(i => ({
+        title: i.name,
+        icon: {
+            path: `images/${i.state === 'EXITING' ? 'EXITING' : 'CONNECTED'}.png`
+        },
+        uid: i.name,
+        arg: i.name,
+        autocomplete: i.name
+    }));
+    alfred.output({items}, ['title']);
+});
